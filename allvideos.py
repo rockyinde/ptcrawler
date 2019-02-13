582e28d6ca16d540e5d8c47d40b4798d8dc7bb91
Rocky <ri.chkmglr@gmail.com>
2019-02-13 18:50:26 +0530

    minor changes

--- allvideos.py
+++ allvideos.py
@@ -17,18 +17,21 @@ YOUTUBE_API_VERSION = "v3"
 # etvcinema = UC3Dvbf4eEov7YORy5nEioTA
 # mallemalatv = UCULLmfWqhMOeiWEV6hkMSCg
 
-playlist_filter_negative = ["song","promos","scene","show","launch","serial","event","live"]
+playlist_filter_negative = ["song","promo","scene","show","launch","serial","event","live","private"]
 playlist_filter_positive = ["full movie"]
+
+vids_file = open('/tmp/videos','w')
+
 '''
 filter whether or not to pass a playlist 
 for further processing
 '''
-def filter (playlist):
+def filter (text):
     for pos in playlist_filter_positive:
-        if pos in playlist['snippet']['title'].lower():
+        if pos in text.lower():
             return True
     for neg in playlist_filter_negative:
-        if neg in playlist['snippet']['title'].lower():
+        if neg in text.lower():
             return False
 
     return True
@@ -58,35 +61,21 @@ def playlists_list_by_channel_id(client):
     request = client.playlists().list_next(
       request, response)
     for playlist in response['items']:
-      if filter(playlist):
+      if filter(playlist['snippet']['title']):
           print 'pass %s (%s)' % (playlist['snippet']['title'], playlist['id'])
+          print '*****************now printing playlist*****************'
+          list_playlist_videos(playlist['id'])
 
-def get_uploads_playlist_id():
-  # Retrieve the contentDetails part of the channel resource for the
-  # authenticated user's channel.
-  channels_response = youtube.channels().list(
-    id='UC_x5XG1OV2P6uZZ5FSM9Ttw' if len(sys.argv) < 2 else sys.argv[1],
-    #id=UCYSr1LyiArqdc_z9nJoCHXw
-    part='contentDetails'
-  ).execute()
-
-  for channel in channels_response['items']:
-    # From the API response, extract the playlist ID that identifies the list
-    # of videos uploaded to the authenticated user's channel.
-    return channel['contentDetails']['relatedPlaylists']['uploads']
-
-  return None
-
-def list_uploaded_videos(uploads_playlist_id):
+def list_playlist_videos(playlist_id):
   # Retrieve the list of videos uploaded to the authenticated user's channel.
   playlistitems_list_request = youtube.playlistItems().list(
-    playlistId=uploads_playlist_id,
+    playlistId=playlist_id,
     part='snippet,contentDetails',
-    fields='items(id, snippet/title, snippet/resourceId/videoId, contentDetails/endAt)',
+    fields='items(id, snippet/title, snippet/resourceId/videoId, contentDetails/endAt),nextPageToken',
     maxResults=25
   )
 
-  print 'Videos in list %s' % uploads_playlist_id
+  print 'Videos in list %s' % playlist_id
   while playlistitems_list_request:
     time.sleep(1)
     playlistitems_list_response = playlistitems_list_request.execute()
@@ -94,9 +83,12 @@ def list_uploaded_videos(uploads_playlist_id):
     # Print information about each video.
     for playlist_item in playlistitems_list_response['items']:
       title = playlist_item['snippet']['title']
+      if not filter(title):
+        continue
       video_id = playlist_item['snippet']['resourceId']['videoId']
       print '%s (%s)' % (title, video_id)
-      print playlist_item
+      vids_file.write('%s %s \n' % (video_id, title))
+      #print playlist_item
 
     playlistitems_list_request = youtube.playlistItems().list_next(
       playlistitems_list_request, playlistitems_list_response)
